280f84d5d6d430c8931a9773f3cafb48d19a91e5
Arnesh Das <akdusa20@gmail.com>
2022-06-13 16:54:09 -0400

    added all of the files from the course

---
+++ CourseCode/Decimals.java
@@ -0,0 +1,14 @@
+package JavaCourse.CourseCode;
+
+public class Decimals {
+    public static void main(String[] args) {
+        //Use double for math
+        double percentage = 55.7;
+        System.out.println("In 2020, " + percentage + " percent of the worlds population is urban");
+
+
+        double dividend = 25;
+        double divisor = 2;
+        System.out.println(dividend + " divided by " + divisor + " is equal to " + dividend/divisor);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JAVA-course
+This is where I do all of my Java Course things. I will add files of new code that I learn during the course. 
