fa9ffa27df1f9f79dc5f15c5f327dfe19e1d3b5d
unknown <akdusa20@gmail.com>
2022-07-03 15:38:23 -0400

    completed this lesson

--- Section4/CourseCode/ReturnValues.java
+++ Section4/CourseCode/ReturnValues.java
@@ -5,24 +5,28 @@ public class ReturnValues {
         Scanner scan = new Scanner(System.in);
 
         
+        double measure1 = measureRectangle(4.3, 2.2, "area");
+        double measure2 = measureRectangle(3.2, 4.1, "perimeter");
+        
+        stringPrinter(4.3, 2.2, measure1, "area");
+        stringPrinter(3.2, 4.1, measure2, "perimeter");
 
-
-        System.out.println("What is your length: ");
-        double length = scan.nextDouble();
-
-        System.out.println("What is your width: ");
-        double width = scan.nextDouble();
-
-        double measure = measureRectangle(length, width);
-
-        System.out.println("The area is " + measure);
-
+        
         scan.close();
     }
 
-    public static double measureRectangle(double length, double width){
-        double area = length * width;
-        return area;
+    public static double measureRectangle(double length, double width, String option){
+        switch (option) {
+            case "area": return length * width; 
+            case "perimeter": return 2 * (length + width);
+            default: return 404;
+
+        }
+    }
+
+    public static void stringPrinter(double length, double width, double area, String option){
+        System.out.println("The " + option + " of a rectangle with length " + length + 
+        " and with width " + width + " is equal to " + area + "\n");
     }
 
 }
