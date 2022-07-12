121781251a7c29d8fba06ef90262a1b3d82fddf6
unknown <akdusa20@gmail.com>
2022-07-12 13:33:02 -0400

    partially finished

---
+++ SectionSix/CourseCode/ArraysAndLoops.java
@@ -0,0 +1,9 @@
+public class ArraysAndLoops {
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "E A"};
+
+        for (int i = 0; i < 4; i++){
+            System.out.println("The element at the index " + i + " is " + kingdoms[i]);
+        }
+    }
+}
