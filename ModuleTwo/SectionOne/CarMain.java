7b5e948cdaae2bcdc5ae49ff7b81cc0c11908b64
unknown <akdusa20@gmail.com>
2022-07-21 15:07:22 -0400

    learned getters

--- ModuleTwo/SectionOne/CarMain.java
+++ ModuleTwo/SectionOne/CarMain.java
@@ -2,11 +2,16 @@ public class CarMain {
     public static void main(String[] args) {
         Car nissan = new Car("Nissan", 5000, 2020, "Red");
         Car dodge = new Car("Dodge", 11000, 2019, "Black");
+ 
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was made in "
+        + nissan.getYear() + " and is " + nissan.getColor());
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price + ". It was made in "
-        + nissan.year + " and is " + nissan.color);
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was made in "
+         + dodge.getYear() + " and is " + dodge.getColor());
 
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price + ". It was made in "
-        + dodge.year + " and is " + dodge.color);
+
     }
+
+    
+
 }
